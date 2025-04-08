c181ea4c6f99668452f6642a30f3d0a0cac3eda1
vktrung <trung040102@gmail.com>
2025-04-09 03:26:22 +0700

    fix form component

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -5,46 +5,51 @@ import clsx from 'clsx';
 import React, { forwardRef } from 'react';
 import { FormProvider, UseFormReturn } from 'react-hook-form';
 
-export interface FormInterface {
+export interface FormProps {
   children: React.ReactNode;
   onSubmit: (data: Record<string, any>) => void;
-  formMethods: UseFormReturn; // Updated to directly accept formMethods
+  formMethods: UseFormReturn;
+  title?: string;
+  description?: string;
+  className?: string;
+  classNameForm?: string;
   [key: string]: any;
 }
 
-const Form = (
-  { children, onSubmit, formMethods, ...props }: FormInterface,
-  ref: React.Ref<HTMLFormElement>,
-) => {
-  return (
-    <div className={clsx('w-full', props.className)}>
-      {props?.title && (
-        <Text as={'h2'} className={clsx(props.description ? 'mb-2' : 'mb-10')}>
-          {props.title}
-        </Text>
-      )}
-      {props?.description && (
-        <Text
-          as={'p'}
-          className="mb-10 font-normal text-[#00000066] text-center text-sm"
-        >
-          {props.description}
-        </Text>
-      )}
-      <FormProvider {...formMethods}>
-        <form
-          noValidate
-          onSubmit={e => {
-            formMethods.handleSubmit(onSubmit)(e);
-          }}
-          className={clsx('w-full', props.classNameForm)}
-          ref={ref}
-        >
-          {children}
-        </form>
-      </FormProvider>
-    </div>
-  );
-};
+const Form = forwardRef<HTMLFormElement, FormProps>(
+  ({ children, onSubmit, formMethods, ...props }, ref) => {
+    return (
+      <div className={clsx('w-full', props.className)}>
+        {props?.title && (
+          <Text as={'h2'} className={clsx(props.description ? 'mb-2' : 'mb-10')}>
+            {props.title}
+          </Text>
+        )}
+        {props?.description && (
+          <Text
+            as={'p'}
+            className="mb-10 font-normal text-[#00000066] text-center text-sm"
+          >
+            {props.description}
+          </Text>
+        )}
+        <FormProvider {...formMethods}>
+          <form
+            noValidate
+            onSubmit={e => {
+              formMethods.handleSubmit(onSubmit)(e);
+            }}
+            className={clsx('w-full', props.classNameForm)}
+            ref={ref}
+          >
+            {children}
+          </form>
+        </FormProvider>
+      </div>
+    );
+  }
+);
 
-export default forwardRef<HTMLFormElement, FormInterface>(Form);
+Form.displayName = 'Form';
+
+export default Form;

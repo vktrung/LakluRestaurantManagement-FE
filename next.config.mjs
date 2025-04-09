920ffb3a6eb7f8c32a7db162525089e6fabc8619
vktrung <trung040102@gmail.com>
2025-04-09 10:39:53 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,39 @@ const nextConfig = {
   images: {
     domains: ['qr.sepay.vn'], // Thêm hostname của URL hình ảnh
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.laklu.com/api/:path*',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
